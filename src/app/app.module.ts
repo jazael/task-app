22d301586097d8756f6deb687d7b7db0af4f886e
miguel.faubla <jazael.faubla@jazael.faubla>
2019-01-15 16:16:53 -0500

    Configuración y estructura inicial del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
+import { APP_ROUTING } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { TaskComponent } from './components/task/task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    TaskComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
